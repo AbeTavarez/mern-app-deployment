0d0a7c8c22a3db2ca1915cd9445c08c621562fd4
Abraham E. Tavarez <abeincwireless@gmail.com>
2023-04-28 11:02:35 -0400

    heroku

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ require('dotenv').config();
 require('./config/database'); // connects to db
 const express = require('express');
 const path = require('path'); // node module
-// const logger = require('morgan');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
 
 const app = express();
 // development port: 3001
@@ -13,10 +14,13 @@ const PORT = process.env.PORT || 3001;
 
 //* Config
 // Logger middleware
-// app.use(logger('dev'));
+app.use(logger('dev'));
 // JSON payload middleware (for data coming from frontend functions)
 app.use(express.json());
-
+// Configure both serve-favicon & static middleware
+// to serve from the production 'build' folder
+app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
+app.use(express.static(path.join(__dirname, 'build')));
 // checks if token was sent and sets a user data on the req (req.user)
 app.use(require('./config/checkToken'));
 
@@ -28,7 +32,9 @@ app.use('/api/users', require('./routes/api/users'));
 // Put API routes here, before the "catch all" route
 // The following "catch all" route (note the *) is necessary
 // to return the index.html on all non-AJAX requests
-
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'))
+})
 
 
 app.listen(PORT, () => {
